abfa96fe27acd807601f2dfd94092ace78634e40
Adrian Figueroa <adrianffigueroa@gmail.com>
2025-05-30 11:59:14 -0300

    links en home

--- src/components/Home/SmallCards.jsx
+++ src/components/Home/SmallCards.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 const SmallCards = ({ icon: Icon, value, label, isLoading, isError }) => {
   let displayValue = value
 
@@ -6,21 +7,23 @@ const SmallCards = ({ icon: Icon, value, label, isLoading, isError }) => {
   else if (typeof value === 'object') displayValue = '?'
 
   return (
-    <div
-      className="group relative gap-4 flex items-center justify-center h-16 w-54 
+    <Link to={label.toLowerCase()}>
+      <div
+        className="group relative gap-4 flex items-center justify-center h-16 w-54 
     rounded-2xl shadow-[0_20px_80px_rgba(74,58,255,0.15)] bg-white
     text-gray-500
     hover:cursor-pointer hover:bg-primary hover:text-white"
-    >
-      <Icon
-        size={30}
-        className="absolute -translate-x-27 text-white bg-third rounded-full p-1"
-      />
-      <span className="text-primary font-semibold text-3xl group-hover:text-white">
-        {displayValue}
-      </span>
-      <span>{label}</span>
-    </div>
+      >
+        <Icon
+          size={30}
+          className="absolute -translate-x-27 text-white bg-third rounded-full p-1"
+        />
+        <span className="text-primary font-semibold text-3xl group-hover:text-white">
+          {displayValue}
+        </span>
+        <span>{label}</span>
+      </div>
+    </Link>
   )
 }
 
